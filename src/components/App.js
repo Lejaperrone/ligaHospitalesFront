6c6112aa22063303082fccdbf1aa1a413e7f040f
Alejo <lejaperrone@gmail.com>
2019-04-05 15:19:13 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return(
+            <div> Hola Liga Inter Hospitales! </div>
+        );
+    }
+}
+
+export default App;
